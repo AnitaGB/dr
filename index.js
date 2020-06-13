c4c6e172eca71ff5b082905e5898960346187859
AnitaGB <63008339+AnitaGB@users.noreply.github.com>
2020-06-13 17:49:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+//add sound to buttons
+let allButtons = document.querySelectorAll(".drum");
+for (let i=0; i<allButtons.length; i++){
+    allButtons[i].addEventListener("click", function () {
+        let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+    });
+}
+//add sound to keys
+document.addEventListener("keydown", function() {
+    makeSound(event.key);
+    }
+);
+//function
+function makeSound(key){
+    switch (key) {
+        case "w":
+                const tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+            break;
+
+            case "a":
+                const tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+            break;
+
+            case "s":
+                const tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+            break;
+
+            case "d":
+                const tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+            break;
+
+            case "j":
+                const crash = new Audio('sounds/crash.mp3');
+                crash.play();
+            break;
+
+            case "k":
+                const snare = new Audio('sounds/snare.mp3');
+                snare.play();
+            break;
+
+            case "l":
+                const kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+            break;
+    }
+}
